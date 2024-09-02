345bc9735fda758b0777b0fcf803a9205af55ed0
melancholicst <dragonsteve304@gmail.com>
2024-09-02 12:38:27 +0300

    Created Patch methods: edit name, edit description

--- MyContacts/Controllers/ContactController.cs
+++ MyContacts/Controllers/ContactController.cs
@@ -14,26 +14,26 @@ namespace MyContacts.Controllers;
 [Authorize]
 [Route("/contact/")]
 [OpenApiRule()]
-public class ContactController(IContactService contactService) : ControllerBase
+public class ContactController(IContactService contactService, ILogger<ContactController> logger) : ControllerBase
 {
     /// <summary>
     ///    Gets from db contact
     /// </summary>
     /// <param name="id">id of contact to get</param>
-    /// <returns>The DTO object of contact DAO</returns>
+    /// <returns>ContactDTO</returns>
     // Crud
     [HttpGet("get/{id}")]
-    public ContactDTO GetContact(int id)
+    public IActionResult GetContact(int id)
     {
         Contact contact;
         try
         {
             contact = contactService.GetContactUser(id);
         }
-        catch (NullReferenceException e)
+        catch (ApplicationException e)
         {
             Console.WriteLine(e);
-            return null;
+            return BadRequest(e.Message);
         }
 
         ContactDTO result = new ContactDTO
@@ -43,6 +43,63 @@ public class ContactController(IContactService contactService) : ControllerBase
             contact.Description,
             contact.Contacts.Select(c => c.Id)
         );
-        return result;
+        return Ok(result);
+    }
+
+    [HttpPatch("edit/name")]
+    public IActionResult EditContactName(int id, string name)
+    {
+        Contact contact;
+        try
+        {
+            contact = contactService.GetContactUser(id);
+        }
+        catch (ApplicationException e)
+        {
+            logger.LogWarning(e.Message);
+            return BadRequest(e);
+        }
+
+        contact.Name = name;
+        try
+        {
+            contactService.UpdateContactUser(contact);
+        }
+        catch (ApplicationException e)
+        {
+            logger.LogWarning(e.Message);
+            return BadRequest(e);
+        }
+
+        return Ok();
+    }
+
+    [HttpPatch("edit/description")]
+    public IActionResult EditContactDescription(int id, string description)
+    {
+        Contact contact;
+        try
+        {
+            contact = contactService.GetContactUser(id);
+        }
+        catch (ApplicationException e)
+        {
+            logger.LogWarning(e.Message);
+            return BadRequest(e);
+        }
+
+        contact.Description = description;
+        
+        try
+        {
+            contactService.UpdateContactUser(contact);
+        }
+        catch (ApplicationException e)
+        {
+            Console.WriteLine(e);
+            return BadRequest(e);
+        }
+
+        return Ok();
     }
 }
