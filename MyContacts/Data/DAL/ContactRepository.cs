5d475b6bb088792f2c94ad3464a19460a3915125
melancholicst <dragonsteve304@gmail.com>
2024-09-02 19:02:13 +0300

    SubContact operations implemented (NOT TESTED exclude InsertSubContact)

--- MyContacts/Data/DAL/ContactRepository.cs
+++ MyContacts/Data/DAL/ContactRepository.cs
@@ -13,12 +13,12 @@ public class ContactRepository(ContactContext context) : IContactRepository
 
     public Contact? GetContactByID(int id)
     {
-        return context.Contacts.Find(id);
+        return context.Contacts.Include(c => c.SubContacts).FirstOrDefault(x => x.Id == id);
     }
 
     public Contact? GetContactByName(string name)
     {
-        return context.Contacts.FirstOrDefault(contact => contact.Name == name);
+        return context.Contacts.Include(c => c.SubContacts).FirstOrDefault(contact => contact.Name == name);
     }
 
     public Contact? GetContactByPhoneNumber(string phoneNumber)
@@ -31,6 +31,38 @@ public class ContactRepository(ContactContext context) : IContactRepository
         context.Contacts.Add(contact);
         Save();
     }
+
+    public void InsertSubContact(Contact contact, Contact subContact)
+    {
+        Contact dbContact = context.Contacts
+            .Include(c => c.SubContacts)
+            .FirstOrDefault(c => c == contact);
+        dbContact.SubContacts.Add(subContact);
+        context.SaveChanges();
+    }
+
+    public void RemoveSubContact(Contact contact, Contact subContact)
+    {
+        Contact dbContact = context.Contacts
+            .Include(c => c.SubContacts)
+            .FirstOrDefault(c => c == contact);
+        dbContact.SubContacts.Remove(subContact);
+        context.SaveChanges();
+    }
+
+    public void UpdateSubContact(Contact contact, Contact subContact)
+    {
+        Contact existingContact = context.Contacts
+            .Include(c => c.SubContacts)
+            .FirstOrDefault(c => c == contact);
+
+        Contact editingContact = existingContact.SubContacts.First(c => c.Id == subContact.Id);
+        
+        context.Entry(editingContact).CurrentValues.SetValues(subContact);
+        context.SaveChanges();
+    }
+    
+
 
     public void UpdateContact(Contact contact)
     {
@@ -49,7 +81,6 @@ public class ContactRepository(ContactContext context) : IContactRepository
     }
     public void Dispose()
     {
-        
         GC.SuppressFinalize(this);
     }
 
