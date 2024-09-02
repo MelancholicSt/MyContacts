801db84027e51447b5fb33bf3352d74638977410
melancholicst <dragonsteve304@gmail.com>
2024-09-02 19:01:40 +0300

    Renaming: AddContactToContactUser -> AddSubContact RemoveContactFromContactUser -> RemoveSubContact
    Added:
    - Overload of GetContactUser. Now possible to find by phone number
    - EditSubContact method

--- MyContacts/Services/IContactService.cs
+++ MyContacts/Services/IContactService.cs
@@ -10,13 +10,15 @@ public interface IContactService : IDisposable
      * 
      */
     IEnumerable<Contact> GetFamiliarContacts((Contact, Contact) contacts);
-    void AddContactToContactUser(Contact user, Contact contact);
-    void RemoveContactFromContactUser(Contact user, Contact contact);
+    void AddSubContact(Contact user, Contact subContact);
+    void RemoveSubContact(Contact user, Contact subContact);
+    void EditSubContact(Contact user, Contact subContact);
 
     
     bool IsPhoneNumberFormatValid(string number);
     // CRUD operations
     Contact GetContactUser(int id);
+    Contact GetContactUser(string phoneNumber);
     void CreateContactUser(Contact user);
     void DeleteContactUser(Contact user);
     void UpdateContactUser(Contact user);
